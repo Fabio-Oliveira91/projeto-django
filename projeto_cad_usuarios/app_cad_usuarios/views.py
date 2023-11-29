3f0ca5d0455b274350fe02756d3ee4e124cc12ad
Fabio Diego <fabiodiego91@gmail.com>
2023-11-29 20:04:38 -0300

    criando pag de usuario

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Usuario
 
 
 def home(request):
@@ -8,4 +7,14 @@ def home(request):
 
 
 def usuarios(request):
-    pass
+    # Salva os dados da tela para o BD
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+    # Exibir todos os usuários já cadastrado em uma nova página
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    # Retorna os dados para a pagina de listagem de usuários
+    return render(request, 'usuarios/usuarios.html', usuarios)
